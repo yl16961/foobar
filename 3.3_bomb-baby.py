df9cac3f5b73b06170099ac524e3a134b1f7c1fe
Yichun Li <yichunli@Yichuns-MacBook-Air-2.local>
2023-09-03 23:05:44 -0700

    All test cases passed

---
+++ 3.3_bomb-baby.py
@@ -0,0 +1,36 @@
+import sys 
+
+def solution(x, y):
+    # Your code here
+    x = int(x)
+    y = int(y)
+    
+    if x == 1 and y == 1:
+        return 0 #edge case 
+    
+    if x%2 == 0 and y%2 == 0: 
+        return "impossible" #if both are even -> impossible
+    
+    #assign bigger and smaller values
+    bigger = x if x > y else y 
+    smaller = x if x < y else y 
+
+    counter = 0 
+    while(bigger > 1):
+        counter += bigger/smaller #number of times we can subtract smaller from bigger 
+        temp = bigger%smaller #remainder of ^ 
+
+        #reassign bigger and smaller
+        bigger = temp if temp > smaller else smaller 
+        smaller = temp if temp < smaller else smaller
+
+        if smaller == 0 and bigger != 1: #if we "ran out" of one bomb before the other reaches 1 -> impossible
+            return "impossible"
+        
+    return str(counter-1) #it'll count an extra step at the end so remove that 
+
+print(solution(1, 1)) #0 
+print(solution('4', '7')) #4
+print(solution('2', '4')) #impossible 
+print(solution('53', '36')) #12
+print(solution('531', '36')) #impossible 
